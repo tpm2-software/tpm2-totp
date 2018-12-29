65fa75c5da09928b796ead68ccd6849aa66703d0
Jonas Witschel <diabonas@gmx.de>
2018-12-29 14:22:02 +0100

    Test whether the secret is sealed against the correct PCRs
    Signed-off-by: Jonas Witschel <diabonas@gmx.de>

--- test/tpm2-totp.sh
+++ test/tpm2-totp.sh
@@ -65,12 +65,32 @@ prepare
 
 ./tpm2-totp -P abc -p 0,1,2,3,4,5,6 -b SHA1,SHA256 generate
 
+# Changing an unselected PCR bank should not affect the TOTP calculation
+tpm2_pcrextend -T mssim 0:sha384=000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+
 ./tpm2-totp -t calculate
+
+tpm2_pcrextend -T mssim 1:sha1=0000000000000000000000000000000000000000
+
+if ./tpm2-totp -t calculate; then
+    echo "The TOTP was calculated despite a changed PCR state!"
+    exit 1
+fi
 
 ./tpm2-totp -P abc recover
 
-./tpm2-totp -P abc reseal
+./tpm2-totp -P abc -p 0,1,2,3,4,5,6 -b SHA1,SHA256 reseal
+
+# Changing an unselected PCR bank should not affect the TOTP calculation
+tpm2_pcrextend -T mssim 0:sha384=000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
 
 ./tpm2-totp calculate
+
+tpm2_pcrextend -T mssim 1:sha1=0000000000000000000000000000000000000000
+
+if ./tpm2-totp calculate; then
+    echo "The TOTP was calculated despite a changed PCR state!"
+    exit 1
+fi
 
 ./tpm2-totp clean
