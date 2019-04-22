ddaa16cfdda34e77cdec7b35da4de2ff28123c03
Jonas Witschel <diabonas@gmx.de>
2019-04-22 12:48:33 +0200

    test: make the integration tests use a TPM simulator
    Currently the integration tests use the first TPM they find on the system.
    This commit starts a TPM simulator for every integration test and
    configures the TCTI environment variable to use it, allowing parallel
    execution of the tests.
    
    The test harness is adapted from the one in tpm2-tss-engine.
    
    Signed-off-by: Jonas Witschel <diabonas@gmx.de>

---
+++ test/sh_log_compiler.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+export LANG=C
+export PATH="$PWD:$PATH"
+
+test_script="$(realpath "$1")"
+
+tmp_dir="$(mktemp --directory)"
+echo "Switching to temporary directory $tmp_dir"
+cd "$tmp_dir"
+
+for attempt in $(seq 9 -1 0); do
+    tpm_server_port="$(shuf --input-range 1024-65534 --head-count 1)"
+    echo "Starting simulator on port $tpm_server_port"
+    tpm_server -port "$tpm_server_port" &
+    tpm_server_pid="$!"
+    sleep 1
+
+    if ( ss --listening --tcp --ipv4 --processes | grep "$tpm_server_pid" | grep --quiet "$tpm_server_port" &&
+         ss --listening --tcp --ipv4 --processes | grep "$tpm_server_pid" | grep --quiet "$(( tpm_server_port + 1 ))" )
+    then
+        echo "Simulator with PID $tpm_server_pid started successfully"
+        break
+    else
+        echo "Failed to start simulator, the port might be in use"
+        kill "$tpm_server_pid"
+
+        if [ "$attempt" -eq 0 ]; then
+            echo 'ERROR: Reached maximum number of tries to start simulator, giving up'
+            exit 99
+        fi
+    fi
+done
+
+export TPM2TOTP_TCTI="libtss2-tcti-mssim.so.0:port=$tpm_server_port"
+export TPM2TOOLS_TCTI="$TPM2TOTP_TCTI"
+
+tpm2_startup --clear
+
+echo "Starting $test_script"
+"$test_script"
+test_status="$?"
+
+kill "$tpm_server_pid"
+rm -rf "$tmp_dir"
+
+exit "$test_status"
--- test/tpm2-totp.sh
+++ test/tpm2-totp.sh
@@ -16,81 +16,43 @@ set -x
 export TSS2_LOG=all+none
 #export TSS2_LOG=esys+trace
 
-TPMSIM=tpm_server
-
 PWD1="abc"
-
-function prereq()
-{
-    if [ -f NVChip ]; then
-        echo "There is a leftover file NVChip in the test directory."
-        return 99 #ERROR
-    fi
-
-    if killall -0 $(basename $TPMSIM); then
-        echo "There is already a tpm_simulator running."
-        return 99 #ERROR
-    fi
-
-    trap "cleanup" EXIT
-}
-
-function prepare()
-{
-    $TPMSIM &
-}
-
-function cleanup()
-{
-    killall $(basename $TPMSIM) || true
-    rm NVChip || true
-    echo .
-}
 
 function error()
 {
     echo "FAILED"
 }
 
-function fail()
-{
-    return 1
-}
-
 trap "error" ERR
 
-prereq
-
-prepare
-
-./tpm2-totp -P abc -p 0,1,2,3,4,5,6 -b SHA1,SHA256 generate
+tpm2-totp -P abc -p 0,1,2,3,4,5,6 -b SHA1,SHA256 generate
 
 # Changing an unselected PCR bank should not affect the TOTP calculation
-tpm2_pcrextend -T mssim 0:sha384=000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+tpm2_pcrextend 0:sha384=000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
 
-./tpm2-totp -t calculate
+tpm2-totp -t calculate
 
-tpm2_pcrextend -T mssim 1:sha1=0000000000000000000000000000000000000000
+tpm2_pcrextend 1:sha1=0000000000000000000000000000000000000000
 
-if ./tpm2-totp -t calculate; then
+if tpm2-totp -t calculate; then
     echo "The TOTP was calculated despite a changed PCR state!"
     exit 1
 fi
 
-./tpm2-totp -P abc recover
+tpm2-totp -P abc recover
 
-./tpm2-totp -P abc -p 0,1,2,3,4,5,6 -b SHA1,SHA256 reseal
+tpm2-totp -P abc -p 0,1,2,3,4,5,6 -b SHA1,SHA256 reseal
 
 # Changing an unselected PCR bank should not affect the TOTP calculation
-tpm2_pcrextend -T mssim 0:sha384=000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+tpm2_pcrextend 0:sha384=000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
 
-./tpm2-totp calculate
+tpm2-totp calculate
 
-tpm2_pcrextend -T mssim 1:sha1=0000000000000000000000000000000000000000
+tpm2_pcrextend 1:sha1=0000000000000000000000000000000000000000
 
-if ./tpm2-totp calculate; then
+if tpm2-totp calculate; then
     echo "The TOTP was calculated despite a changed PCR state!"
     exit 1
 fi
 
-./tpm2-totp clean
+tpm2-totp clean
