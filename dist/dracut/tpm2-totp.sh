b42be18287d0e000e1582eae6dd8634d8ca7884d
Jonas Witschel <diabonas@gmx.de>
2019-07-13 13:45:47 +0200

    dist: install tss2-tcti-device to initramfs
    Since https://github.com/tpm2-software/tpm2-tss/commit/b258ab9618ded645777917a742b917a3ba72f107
    libtss2-esys.so does not directly depend on libtss2-tcti-device.so any
    more, so we need to install the TCTI library manually to the initramfs.
    
    The same commit moves the warning about missing TCTI libraries from the
    esys to the tcti module, so the TSS2_LOG environment variable needs to
    be updated as well.
    
    Signed-off-by: Jonas Witschel <diabonas@gmx.de>

--- dist/dracut/tpm2-totp.sh
+++ dist/dracut/tpm2-totp.sh
@@ -2,5 +2,5 @@
 . /lib/dracut-lib.sh
 nvindex="$(getarg rd.tpm2-totp.nvindex)"
 export TSS2_LOG
-printf 'KERNEL=="tpm0", RUN+="/sbin/initqueue --settled --onetime --env TSS2_LOG=esys+error /bin/plymouth-tpm2-totp %s &"\n' "${nvindex:+--nvindex "$nvindex"}" > /etc/udev/rules.d/80-tpm2-totp.rules
+printf 'KERNEL=="tpm0", RUN+="/sbin/initqueue --settled --onetime --env TSS2_LOG=tcti+error /bin/plymouth-tpm2-totp %s &"\n' "${nvindex:+--nvindex "$nvindex"}" > /etc/udev/rules.d/80-tpm2-totp.rules
 unset nvindex
