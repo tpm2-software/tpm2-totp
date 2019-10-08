25b95eccbd838927e6b32b9ec194d37e9faa9147
Jonas Witschel <diabonas@gmx.de>
2019-10-08 10:34:23 +0200

    dist: add option to display TOTP without plymouth in dracut
    show-tpm2-totp from the previous commit can be used for dracut as well.
    
    Signed-off-by: Jonas Witschel <diabonas@gmx.de>

--- dist/dracut/tpm2-totp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-. /lib/dracut-lib.sh
-nvindex="$(getarg rd.tpm2-totp.nvindex)"
-printf 'KERNEL=="tpm0", RUN+="/sbin/initqueue --settled --onetime /bin/plymouth-tpm2-totp %s &"\n' "${nvindex:+--nvindex "$nvindex"}" > /etc/udev/rules.d/80-tpm2-totp.rules
-unset nvindex
